438a4e12e3fdf8569630e9d4afc25bc679919c1d
Pablo Ortega <p.ortegariera@gmail.com>
2023-01-19 15:29:41 -0300

    feat: Agrego estilos globales, tema oscuro y coloco tema en componento login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,28 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import './App.css';
 import Chat from './pages/chat/Chat';
 import SetAvatar from './pages/setAvatar/SetAvatar';
 import Spinner from './components/spinner/Spinner';
+import './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import dark from './styles/themes/dark';
+import GlobalStyle from './styles/GlobalStyle';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/setAvatar" element={<SetAvatar />} />
-        <Route path="/spinner" element={<Spinner />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={dark}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+          <Route path="/spinner" element={<Spinner />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
---
+++ client/src/styles/themes/dark.ts
@@ -0,0 +1,28 @@
+export default {
+  title: 'light',
+  colors: {
+    logo: '#0380F4',
+
+    primary100: '#1bb4e7',
+    primary80: '#1bb4e780',
+    primary40: '#1bb4e740',
+
+    secondary100: '#a663cc',
+    secondary80: '#a663cc80',
+    secondary40: '#a663cc40',
+
+    green: '#80D62F',
+
+    background: {
+      '1': '#383838',
+      '3': '#262626',
+      '2': '#2e2e2e'
+    },
+    text: {
+      '1': '#F0F0F0',
+      '2': '#CBCBCB',
+      '3': '#7C7C7F',
+      link: '#0063F4'
+    }
+  }
+};
